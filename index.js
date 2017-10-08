8f84f779c2f3da60716b0d742b93a7c79d7a1b64
tzmanics <tzmanics@gmail.com>
2017-10-08 16:51:30 -0400

    remove newAmbianceRef & round up data output

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ const tessel = require('tessel');
 const ambient = ambientlib.use(tessel.port['A']);
 
 const config = {
-  apiKey: "<enter apiKey",
-  authDomain: "<enter authDomain>",
-  databaseURL: "<enter databaseURL>",
-  projectId: "<enter projectId>",
-  storageBucket: "<enter storageBuckets>",
-  messagingSenderId: "<enter messagingSenderId>"
+  apiKey: "<insert firebase credentials>",
+  authDomain: "<insert firebase credentials>",
+  databaseURL: "<insert firebase credentials>",
+  projectId: "<insert firebase credentials>",
+  storageBucket: "<insert firebase credentials>",
+  messagingSenderId: "<insert firebase credentials>"
 };
 
 firebase.initializeApp(config);
 
 const db = firebase.database();
-const ref = db.ref("sensor-data");
-const ambianceRef = ref.child("ambiance");
-const newAmbianceRef = ambianceRef.push();
+const ref = db.ref('sensor-data');
 
 ambient.on('ready', () => {
   setInterval(() => {
@@ -26,12 +24,11 @@ ambient.on('ready', () => {
       if (err) throw err;
       ambient.getSoundLevel( (err, sounddata) => {
         if (err) throw err;
-        newAmbianceRef.set({
-          light: lightdata.toFixed(4),
-          sound: sounddata.toFixed(4)
-        })
-        console.log('Light level:', lightdata.toFixed(4));
-        console.log('Sound level:', sounddata.toFixed(4));
+        const ambienceRef = ref.child('ambience/' + Date.now());
+        ambienceRef.set({
+          light: lightdata.toFixed(4) * 100,
+          sound: sounddata.toFixed(4) * 100
+        });
       });
     });
   }, 30000);
